61eaa99f5599304d67cbf9e943300f76567a1220
Samuel <samuelcauabarbosa114@gmail.com>
2022-07-11 18:43:49 -0300

    Atualização dia 11?07 as 18:44

--- main.js
+++ main.js
@@ -17,3 +17,20 @@ function movePointer(e) {
   var order = e.currentTarget.dataset.order;
   pointer.style.transform = "translate3d(" + order + ",0,0)";
 }
+
+
+setInterval(function () {
+  const show = document.querySelector("span[data-show]");
+  const next =
+    show.nextElementSibling || document.querySelector("span:first-child");
+  const up = document.querySelector("span[data-up]");
+
+  if (up) {
+    up.removeAttribute("data-up");
+  }
+
+  show.removeAttribute("data-show");
+  show.setAttribute("data-up", "");
+
+  next.setAttribute("data-show", "");
+}, 2000);
